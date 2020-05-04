98fe4ce64fb813a5c4a62863f525fdf93d6c162e
anbreaker <antunez19@gmail.com>
2020-05-04 13:49:23 +0200

    work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    purge: [],
-    theme: {
-        extend: {},
-    },
-    variants: {},
-    plugins: [],
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+  },
+  plugins: [],
 };
